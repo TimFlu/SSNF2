3b2e5f8324a1fcd3ec0cbf9937355c64b607bbdd
Tim Gabriel Fluehmann (ETHZ) [tfluehma] <tfluehma@t3gpu02.psi.ch>
2023-11-22 17:06:10 +0100

    full impementation, still debugging

--- classifier/classifier_2.py
+++ classifier/classifier_2.py
@@ -23,8 +23,8 @@ class CustomDataset(Dataset):
         return x, y
 
 # Create training and test data
-train_data = pd.read_parquet("SSNF2/classifier/data/train_data.parquet")
-test_data = pd.read_parquet("SSNF2/classifier/data/test_data.parquet")
+train_data = pd.read_parquet("/work/tfluehma/git/SSNF2/classifier/data/train_data.parquet")
+test_data = pd.read_parquet("/work/tfluehma/git/SSNF2/classifier/data/test_data.parquet")
 
 train = train_data.iloc[:, :-1]
 train_label = train_data["label"]
@@ -36,10 +36,10 @@ train_dataset = CustomDataset(train, train_label)
 test_dataset = CustomDataset(test, test_label)
 
 # Create Dataloader
-train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
+batch_size=64
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 
-print(len(train_dataloader))
 
 # ************** Build the Neural Network ******************
 # Get device for training
@@ -52,10 +52,86 @@ device = (
 )
 print(f"Using {device} device")
 
+
+len = len(train_dataloader)
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
-        self.flatten = nn.Flatten()
+        # self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear()
+            nn.Linear(len, 20),
+            nn.ReLU(),
+            nn.Linear(20, 20),
+            nn.ReLU(),
+            nn.Linear(20, 1),
+            nn.Sigmoid()
         )
+    
+    def forward(self, x):
+        # x = self.fatten(x)
+        x = self.linear_relu_stack(x)
+        return x
+    
+model = NeuralNetwork().to(device)
+print(model)
+
+# Define Hyperparameters
+learning_rate = 1e-3
+epochs = 5
+batch_size = 64
+
+
+
+# **************** Train Function **************** #
+def train_loop(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    # Set the model to training mode
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        # Compute prediction and loss
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+# **************** Test Function **************** #
+def test_loop(dataloader, model, loss_fn):
+    # Set the model to evaluation mode
+    model.eval()
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+
+    # Evaluation the model with torch.no_grad() ensures that no gradients
+    # are computed during test mode
+    with torch.no_grad():
+        for X, y in dataloader:
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item
+            correct += (pred.argmax(1)== y).type(torch.float).sum().item()
+
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+    
+# initialize the loss function and optimizer
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+epochs = 10
+
+# ********************* Actual Testing ********************* #
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train_loop(train_dataloader, model, loss_fn, optimizer)
+    test_loop(test_dataloader, model, loss_fn)
+
+print("Done")
