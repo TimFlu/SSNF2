b5d77d584f259a1aad85852cc10e41ad1b82844f
Tim Gabriel Fluehmann (ETHZ) [tfluehma] <tfluehma@t3ui02.psi.ch>
2023-11-29 10:08:59 +0100

    renamed classifier_2 to classifier

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -1,74 +1,193 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
-# LabelEncoder used to convert string label to an integer ** not sure if used **
-from sklearn.preprocessing import LabelEncoder
-
 # torch
 import torch
-import torch.nn as nn
-import torch.optim as optim
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from torch import nn
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import torchvision.models as models
+# other
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
 import copy
+from plots_classifier import plot_loss_function, plot_data
+import pickle as pkl
 
 
-#******* Prepare the data *******#
-data_eb_train = pd.read_parquet("SSNF2/preprocess/data_eb_train.parquet")
-# print(data_eb_train.head())
-# print(data_eb_train.keys())
-# print("shape: ", data_eb_train.shape)
-mc_eb_train = pd.read_parquet("SSNF2/preprocess/mc_eb_train.parquet")
-# print(mc_eb_train.head())
-# print("shape: ", mc_eb_train.shape)
-
-# Create a test dataset just for probe_pt
-data_eb_train["label"] = data_eb_train.shape[0] * ["data"]
-data_probe_pt = data_eb_train[["probe_pt", "label"]]
-
-mc_eb_train["label"] = mc_eb_train.shape[0] * ["mc"]
-mc_probe_pt = mc_eb_train[["probe_pt", "label"]]
-
-probe_pt = pd.concat([data_probe_pt, mc_probe_pt], axis=0)
-probe_pt_train = probe_pt.iloc[:, :-1]
-label = probe_pt.iloc[:, -1]
+# Get device
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+print(f"Using {device} device")
 
 
-# Change string labels to integers mc -> 1, data -> 0
+# ************* Create custom Dataset ************* #
+class CustomDataset(Dataset):
+    def __init__(self, data, labels, pipelines=None):
+        self.data = data
+        self.labels = torch.tensor(labels, dtype=torch.float32, device=device).reshape(-1, 1)
+        self.pipelines = pipelines
+        self.all_variables = ["probe_pt", "probe_eta", "probe_phi", "probe_fixedGridRhoAll", "probe_r9", "probe_s4",
+                              "probe_sieie", "probe_sieip", "probe_etaWidth", "probe_phiWidth", "probe_pfPhoIso03",
+                              "probe_pfChargedIsoPFPV", "probe_pfChargedIsoWorstVtx", "probe_energyRaw"]
+        
+        if self.pipelines is not None:
+            for var, pipeline in self.pipelines.items():
+                if var in self.all_variables:
+                    trans = (
+                        pipeline.transform
+                    )
+                    data[var] = trans(data[var].values.reshape(-1, 1)).reshape(-1)
+
+        self.data = torch.tensor(data.values, dtype=torch.float32, device=device)
+
+    def __len__(self):
+        return self.data.shape[0]
+    
+    def __getitem__(self, index):
+        x = self.data[index]
+        y = self.labels[index]
+        return x, y
+
+# Create training and test data
+train_data = pd.read_parquet("/work/tfluehma/git/SSNF2/classifier/data/train_data.parquet")
+test_data = pd.read_parquet("/work/tfluehma/git/SSNF2/classifier/data/test_data.parquet")
+
+train = train_data.iloc[:, :-1]
+train_label = train_data["label"]
+
+test = test_data.iloc[:, :-1]
+test_label = test_data["label"]
+
+# Change string labels to integers
 encoder = LabelEncoder()
-encoder.fit(label)
-label = encoder.transform(label)
+encoder.fit(train_label)
+train_label = encoder.transform(train_label)
+test_label = encoder.transform(test_label)
 
+# Define the pipelines
+with open("/work/tfluehma/git/SSNF2/preprocess/pipelines_eb.pkl", "rb") as file:
+    pipelines = pkl.load(file)
+    pipelines = pipelines["pipe1"]
 
-# Convert the data into pytorch tensors
-probe_pt_train = torch.tensor(probe_pt_train.values, dtype=torch.float64)
-label = torch.tensor(label, dtype=torch.float64).reshape(-1, 1)
-# We move our tensor to the GPU if available
-if torch.cuda.is_available():
-    probe_pt_train = probe_pt_train.to("cuda")
+# Create Dataset
+train_dataset = CustomDataset(train, train_label, pipelines=pipelines)
+test_dataset = CustomDataset(test, test_label, pipelines=pipelines)
 
+# Plot the data to verify preprocessing worked
+# plot_data(train_dataset.data, keys=train.keys())
 
-#******* Create a Model *******#
-size = len(probe_pt_train)
-print("shape: ", probe_pt_train.shape)
-class Classifier(nn.Module):
+# Create Dataloader
+batch_size = 32
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+
+# ************** Build the Neural Network ******************
+class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
-        self.layer1 = nn.Linear(size, 5)
-        self.act1 = nn.ReLU()
-        self.layer2 = nn.Linear(5, 5)
-        self.act2 = nn.ReLU()
-        self.layer3 = nn.Linear(5, 5)
-        self.act3 = nn.ReLU()
-        self.output = nn.Linear(5, 1)
-        self.sigmoid = nn.Sigmoid()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(14, 400),
+            nn.ReLU(),
+            nn.Linear(400, 400),
+            nn.ReLU(),
+            nn.Linear(400, 400),
+            nn.ReLU(),
+            nn.Linear(400, 400),
+            nn.ReLU(),
+            nn.Linear(400, 400),
+            nn.ReLU(),
+            nn.Linear(400, 400),
+            nn.ReLU(),
+            nn.Linear(400, 1),
+            nn.Sigmoid()
+        )
     
     def forward(self, x):
-        x = self.act1(self.layer1(x))
-        x = self.act2(self.layer2(x))
-        x = self.act3(self.layer3(x))
-        x = self.sigmoid(self.output(x))
+        x = self.linear_relu_stack(x)
         return x
-print(sum([x.reshape(-1).shape[0] for x in Classifier().parameters()]))
+    
+model = NeuralNetwork().to(device)
+print(model)
 
+
+# **************** Train Function **************** #
+def train_loop(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    # Set the model to training mode
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        # Compute prediction and loss
+        # print(X)
+        # print(y.reshape(1, -1))
+        pred = model(X)
+        loss = loss_fn(pred, y)
+        # print(pred.reshape(1, -1))
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 1000 == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            correct = (pred.round() == y).float().mean()
+            print(f"loss: {loss:>7f} Accuracy: {(100*correct):>0.1f}%  [{current:>5d}/{size:>5d}]")
+    return loss.item()
+
+# **************** Test Function **************** #
+def test_loop(dataloader, model, loss_fn):
+    # Set the model to evaluation mode
+    model.eval()
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+
+    # Evaluation the model with torch.no_grad() ensures that no gradients
+    # are computed during test mode
+    with torch.no_grad():
+        for X, y in dataloader:
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.round() == y).float().mean()
+
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    
+    return test_loss
+
+# ******** classifier function containing training, testing and keeping track of results ********
+def classify(train_dataloader, test_dataloader, model, loss_fn, optimizer, epochs=10):
+    # keep track of models loss
+    train_loss_list = []
+    test_loss_list = []
+
+    for t in range(epochs):
+        print(f"Epoch {t+1}\n-------------------------------")
+        train_loss = train_loop(train_dataloader, model, loss_fn, optimizer)
+        test_loss = test_loop(test_dataloader, model, loss_fn)
+
+        train_loss_list.append(train_loss)
+        test_loss_list.append(test_loss)
+    plot_loss_function(training_loss=train_loss_list, testing_loss=test_loss_list)
+    print("Done")
         
-#********* Training function ********#
+
+# ********************* Actual Testing ********************* #
+# Define Hyperparameters
+learning_rate = 1e-3
+epochs = 100
+# initialize the loss function and optimizer
+loss_fn = nn.BCELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+# optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# Run the classifier
+classify(train_dataloader, test_dataloader, model, loss_fn, optimizer, epochs)
